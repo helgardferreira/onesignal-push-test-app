899dd275f7b2f96bcfe2dc2d22493b7ef0802f4f
Helgard Ferreira <hrfer3@gmail.com>
2019-07-14 17:14:51 +0200

    Add OneSignal Integration

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,6 +8,18 @@
 
 <script>
 export default {
-  name: "PageIndex"
+  name: "PageIndex",
+  methods: {
+    handleNotifications() {
+      window.plugins.OneSignal.startInit("51cffe2a-2373-40dc-95f2-610a895c3888")
+        .handleNotificationOpened(payload => {
+          console.log("notificationOpenedCallback: " + JSON.stringify(payload));
+        })
+        .endInit();
+    }
+  },
+  created() {
+    this.handleNotifications();
+  }
 };
 </script>
